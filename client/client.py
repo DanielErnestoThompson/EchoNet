2c2ffdacd386ae4a55094d89634501c3389971de
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-12-28 16:13:12 -0700

    added additional requests from client and request handling on server

--- client/client.py
+++ client/client.py
@@ -2,11 +2,19 @@ import socket
 
 def start_client():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect(('127.0.0.1', 6254)) 
-        message = 'Hello, Server!'
-        s.sendall(message.encode())
-        data = s.recv(1024)
-        print(f"Received: {data.decode()}")
+        s.connect(('127.0.0.1', 6254))
+
+        # Testing new requests on server 
+        requests = [
+            'get_time',
+            'echo: Hello, Server!',
+            'calculate_sum:5,3'
+        ]
+
+        for req in requests:
+            s.sendall(rec.encode())
+            data = s.recv(1024)
+            print(f"Received: {data.decode()}")
 
 if __name__ == "__main__":
     start_client()
--- server/server.py
+++ server/server.py
@@ -1,18 +1,31 @@
 import socket
+from datetime import datetime
+
+def handle_request():
+    if request_type == 'get_time':
+        return (datetime.now())
+    elif request_type == 'echo':
+        return message
+    elif request_type == 'calculate_sum':
+        numbers = map(int, message.split(','))
+        return str(sum(numbers))
+    else: 
+        return 'Uknown request type'
 
 def start_server():
     with socket.socket(socket.AF_INET , socket.SOCK_STREAM) as s:
         s.bind(('127.0.0.1', 6254))
         s.listen()
-        print("Server is listening....")
+        print('Server is listening....')
         conn, addr = s.accept()
         with conn:
-            print(f"Connected to {addr}")
+            print(f'Connected to {addr}')
             while True:
                 data = conn.recv(1024)
                 if not data:
                     break
-                print(f"Received Message: {data.decode()}")
-
-if __name__ == "__main__":
+                request_type, message = data.decode().split(':',1)
+                response = handle_request(request_type, message)
+                conn.sendall(response.encode())
+if __name__ == '__main__':
     start_server()
